0fa8e0db4f4684d2b7abe6c09af1aef4ace5ab1d
Madhukar97 <madhukar.tatiraju@gmail.com>
2022-04-05 03:05:07 +0530

    Added: Find missing and duplicate numbers in 1-n Array

---
+++ src/main/java/com/arrays/SetMissMatch.java
@@ -0,0 +1,35 @@
+package com.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Find the number that is missing and number that is duplicate in 1 to n Array
+public class SetMissMatch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,4};
+        int[] arr1 = {1,1};
+        System.out.println(missingSet(arr));
+    }
+
+    //Finding missing set using cyclic sort
+    static List<Integer> missingSet(int[] arr) {
+        int i=0;
+        List<Integer> missingSet = new ArrayList<>();
+
+        while ( i < arr.length ) {
+            if ( arr[i] != i+1 ) {
+                int correctIndex = arr[i] - 1 ;
+                if ( arr[i] != arr[correctIndex] ) {
+                    int temp = arr[correctIndex];
+                    arr[correctIndex] = arr[i];
+                    arr[i] = temp;
+                } else {
+                    missingSet.add( arr[i] );
+                    missingSet.add( i+1 );
+                    return missingSet;
+                }
+            } else i++;
+        }
+        return missingSet;
+    }
+}
