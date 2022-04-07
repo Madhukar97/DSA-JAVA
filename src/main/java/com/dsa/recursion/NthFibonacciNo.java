13e5b7d8d021da1ad548c3c39d8c5288042e34ee
Madhukar97 <madhukar.tatiraju@gmail.com>
2022-04-07 18:32:09 +0530

    Added: Recursion problems

---
+++ src/main/java/com/dsa/recursion/NthFibonacciNo.java
@@ -0,0 +1,14 @@
+package com.dsa.recursion;
+
+public class NthFibonacciNo {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(7));
+    }
+
+    static int fibonacci(int n) {
+        if( n < 2) return n;
+        return fibonacci(n-1) + fibonacci(n - 2);
+    }
+
+
+}
