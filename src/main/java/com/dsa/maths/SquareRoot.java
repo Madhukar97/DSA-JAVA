8d4b368ac8a4c69a5f59864834dba9c2e764d70c
Madhukar97 <madhukar.tatiraju@gmail.com>
2022-04-11 18:39:02 +0530

    Added: Square root of number using logn time complexity

--- src/main/java/com/dsa/maths/SquareRoot.java
+++ src/main/java/com/dsa/maths/SquareRoot.java
@@ -3,7 +3,7 @@ package com.dsa.maths;
 //square root of a number using binary search
 public class SquareRoot {
     public static void main(String[] args) {
-        int n = 90;
+        int n = 40;
         int precision = 3;
         System.out.println(squareRoot(n, precision));
     }
@@ -12,7 +12,7 @@ public class SquareRoot {
     static double squareRoot(int n, int p) {
         double start=0;
         double end=n;
-        double mid = 0;
+        double mid;
         while (start <= end) {
             mid = (int)(start + (end-start)/2);
             if ( mid*mid == n) return mid;
@@ -53,7 +53,6 @@ public class SquareRoot {
             while ( root*root <= n) {
                 root+=increment;
             }
-
             root-=increment;
             increment/=10;
         }
