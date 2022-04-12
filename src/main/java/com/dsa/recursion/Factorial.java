6340b25c1118e1a3ec803f1db1e8f5a4c18d41a2
Madhukar97 <madhukar.tatiraju@gmail.com>
2022-04-13 00:05:46 +0530

    Added: Factorial of a num using recursion

---
+++ src/main/java/com/dsa/recursion/Factorial.java
@@ -0,0 +1,13 @@
+package com.dsa.recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(factorialOfNum(6));
+    }
+
+    static int factorialOfNum(int n) {
+        if ( n == 1) return 1;
+        return n * factorialOfNum( n-1 );
+    }
+}
+
