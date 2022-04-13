a70585ebe0deb7cf2d955beb29385c7e62a05a54
Madhukar97 <madhukar.tatiraju@gmail.com>
2022-04-13 15:19:58 +0530

    Added: Triangle Pattern using recursion

--- src/main/java/com/dsa/recursion/TrianglePattern.java
+++ src/main/java/com/dsa/recursion/TrianglePattern.java
@@ -3,7 +3,7 @@ package com.dsa.recursion;
 // Print triangle pattern using recursion
 public class TrianglePattern {
     public static void main(String[] args) {
-        pattern1(4,0);
+        pattern2(4,0);
     }
 
     static void pattern1(int r, int c) {
@@ -17,4 +17,14 @@ public class TrianglePattern {
         }
     }
 
+    static void pattern2(int r, int c) {
+        if (r < 0) return;
+        if ( c <= r) {
+            pattern2( r, c+1);
+            System.out.print("* ");
+        }else {
+            pattern2(r-1,0);
+            System.out.println();
+        }
+    }
 }
