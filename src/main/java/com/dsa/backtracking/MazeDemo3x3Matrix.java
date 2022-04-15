244cd0875abfd13806aa929b4a1e581d0fa87dbf
Madhukar97 <madhukar.tatiraju@gmail.com>
2022-04-15 22:21:49 +0530

    Added: Matrix maze demo using recursion

--- src/main/java/com/dsa/backtracking/MazeDemo3x3Matrix.java
+++ src/main/java/com/dsa/backtracking/MazeDemo3x3Matrix.java
@@ -4,10 +4,22 @@ package com.dsa.backtracking;
 public class MazeDemo3x3Matrix {
     public static void main(String[] args) {
         System.out.println(noOfPaths(3,3));
+        printAllPaths("", 3, 3);
     }
 
+    //Method to print the no.of possible paths
     static int noOfPaths(int r, int c) {
         if ( r == 1 || c == 1 ) return 1;
         return noOfPaths(r-1, c) + noOfPaths(r, c-1);
+    }
+
+    //Method to print all the possible paths
+    static void printAllPaths(String processed, int r, int c) {
+        if ( r == 1 && c == 1 ) {
+            System.out.println(processed);
+            return;
+        }
+        if ( r > 1 ) printAllPaths(processed + "D", r-1, c);
+        if ( c > 1 )  printAllPaths(processed + "R", r, c-1);
     }
 }
