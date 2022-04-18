4125d60600c56307f54dd967f8083fae113e4183
Madhukar97 <madhukar.tatiraju@gmail.com>
2022-04-18 13:26:34 +0530

    Added: FInd if Cyclic linkedlist leetcode

---
+++ src/main/java/com/linkedlistproblems/CyclicLinkedList.java
@@ -0,0 +1,25 @@
+package com.linkedlistproblems;
+
+public class CyclicLinkedList {
+    public static void main(String[] args) {
+
+    }
+    public class ListNode {
+        int val;
+        CyclicLinkedList.ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, CyclicLinkedList.ListNode next) { this.val = val; this.next = next; }
+    }
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null) {
+            if(fast == slow) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+}
