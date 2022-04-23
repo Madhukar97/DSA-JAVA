11bd8ee41071598c59c43ede141f8f5e884b4082
Madhukar97 <madhukar.tatiraju@gmail.com>
2022-04-23 13:14:47 +0530

    Added: public int divide(int dividend, int divisor) { leetcode

---
+++ src/main/java/com/math/DivideTwoIntegers.java
@@ -0,0 +1,15 @@
+package com.math;
+
+//29. Divide Two Integers
+//https://leetcode.com/problems/divide-two-integers/
+public class DivideTwoIntegers {
+    public static void main(String[] args) {
+
+    }
+    public int divide(int dividend, int divisor) {
+        long quoe = (long)dividend/divisor;
+        if(quoe > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if(quoe < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        else return (int)quoe;
+    }
+}
