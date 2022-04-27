45a4a7843ff910692401f13d67bf35dc9a134b08
Madhukar97 <madhukar.tatiraju@gmail.com>
2022-04-27 18:33:53 +0530

    Added: Binary tree zigzag level order traversal leetcode

--- src/main/java/com/trees/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/main/java/com/trees/BinaryTreeZigzagLevelOrderTraversal.java
@@ -2,29 +2,31 @@ package com.trees;
 
 import java.util.*;
 
-//102. Binary Tree Level Order Traversal
-//https://leetcode.com/problems/binary-tree-level-order-traversal/
-public class LevelOrderTraversal {
+//103. Binary Tree Zigzag Level Order Traversal
+//https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+public class BinaryTreeZigzagLevelOrderTraversal {
     public static void main(String[] args) {
 
     }
     public class TreeNode {
         int val;
-        LevelOrderTraversal.TreeNode left;
-        LevelOrderTraversal.TreeNode right;
+        BinaryTreeZigzagLevelOrderTraversal.TreeNode left;
+        BinaryTreeZigzagLevelOrderTraversal.TreeNode right;
         TreeNode() {}
         TreeNode(int val) { this.val = val; }
-        TreeNode(int val, LevelOrderTraversal.TreeNode left, LevelOrderTraversal.TreeNode right) {
+        TreeNode(int val, BinaryTreeZigzagLevelOrderTraversal.TreeNode left, BinaryTreeZigzagLevelOrderTraversal.TreeNode right) {
             this.val = val;
             this.left = left;
             this.right = right;
         }
     }
-    public List<List<Integer>> levelOrder(TreeNode root) {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         if(root == null) return new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         queue.add(null);
+        boolean flag = false;
 
         List<List<Integer>> outer  = new ArrayList<>();
         List<Integer> inner = new ArrayList<>();
@@ -55,6 +57,9 @@ public class LevelOrderTraversal {
             }
         }
         outer.add(inner);
+        for(int i=1; i<outer.size(); i=i+2) {
+            Collections.reverse(outer.get(i));
+        }
         return outer;
     }
 }
