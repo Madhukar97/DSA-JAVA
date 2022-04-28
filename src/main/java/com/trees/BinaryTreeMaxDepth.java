617c22792f618b2c747bd8379954b57f3941d580
Madhukar97 <madhukar.tatiraju@gmail.com>
2022-04-28 17:47:50 +0530

    Added: Binary tree max depth leetcode

---
+++ src/main/java/com/trees/BinaryTreeMaxDepth.java
@@ -0,0 +1,29 @@
+package com.trees;
+
+//104. Maximum Depth of Binary Tree
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/
+public class BinaryTreeMaxDepth {
+    public static void main(String[] args) {
+
+    }
+    public class TreeNode {
+        int val;
+        BinaryTreeMaxDepth.TreeNode left;
+        BinaryTreeMaxDepth.TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, BinaryTreeMaxDepth.TreeNode left, BinaryTreeMaxDepth.TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        return recFunc(root);
+    }
+    static int recFunc(TreeNode root) {
+        if(root == null) return 0;
+        return Math.max(recFunc(root.left) + 1, recFunc(root.right) + 1);
+    }
+}
