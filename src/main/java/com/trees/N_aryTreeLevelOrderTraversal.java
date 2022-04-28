241eeeac9436ed4cf265e56480c06ae064a815ba
Madhukar97 <madhukar.tatiraju@gmail.com>
2022-04-28 11:01:34 +0530

    Added: N-ary tree level order traversal leetcode

--- src/main/java/com/trees/N_aryTreeLevelOrderTraversal.java
+++ src/main/java/com/trees/N_aryTreeLevelOrderTraversal.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+//429. N-ary Tree Level Order Traversal
+//https://leetcode.com/problems/n-ary-tree-level-order-traversal/
 public class N_aryTreeLevelOrderTraversal {
     public static void main(String[] args) {
 
