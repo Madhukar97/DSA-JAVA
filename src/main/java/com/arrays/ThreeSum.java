39b44b8195726b159460d8ec8c82e7d13b7cf4e0
Madhukar <madhukar.tatiraju@gmail.com>
2023-10-04 17:15:47 +0530

    Added ThreeSum problem sol 2

--- src/main/java/com/arrays/ThreeSum.java
+++ src/main/java/com/arrays/ThreeSum.java
@@ -72,4 +72,34 @@ public class ThreeSum {
         }
     }
 
+    //Most optimal sol using 2 pointers with time O(n^2) and space O(1)
+    public List<List<Integer>> threeSumOptimal2(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+            while(j < k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum > 0) k--;
+                else if(sum < 0) j++;
+                else{
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[j]);
+                    triplet.add(nums[k]);
+                    ans.add(triplet);
+                    j++;
+                    k--;
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] == nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+
 }
