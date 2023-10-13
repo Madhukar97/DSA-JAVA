ab39ab241118cf0ca31416e7ef64f994594a57da
Madhukar <madhukar.tatiraju@gmail.com>
2023-10-13 20:47:15 +0530

    Added SearchInRotatedSortedArray problem sol

--- src/main/java/com/arrays/SearchInRotatedSortedArray.java
+++ src/main/java/com/arrays/SearchInRotatedSortedArray.java
@@ -23,4 +23,25 @@ public class SearchInRotatedSortedArray {
         }
         else return binarySearch(nums, target, mid+1, e);
     }
+    //Most optimal sol with time O(log(n)) and space O(1)
+    //Either left half or right half of array is always sorted in rotated sorted arrays
+    //Always compare target in the sorted half of array
+    public int searchSol2(int[] nums, int target) {
+        int s=0;
+        int e=nums.length-1;
+
+        while(s <= e){
+            int mid=s+(e-s)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[s] <= nums[mid]){
+                if(nums[s] <= target && target < nums[mid]) e=mid-1;
+                else s=mid+1;
+            }
+            else{
+                if(nums[mid] < target && target <= nums[e]) s=mid+1;
+                else e=mid-1;
+            }
+        }
+        return -1;
+    }
 }
