745ea7411ec1dafcd822118fb644ff3843d227b8
Madhukar <madhukar.tatiraju@gmail.com>
2023-10-18 03:00:53 +0530

    Added BinaryTreePreorderTraversal problem sol

--- src/main/java/com/trees/BinaryTreePreorderTraversal.java
+++ src/main/java/com/trees/BinaryTreePreorderTraversal.java
@@ -2,6 +2,7 @@ package com.trees;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 //144. Binary Tree Preorder Traversal
 public class BinaryTreePreorderTraversal {
@@ -34,5 +35,27 @@ public class BinaryTreePreorderTraversal {
         ans.add(root.val);
         recFunc(root.left, ans);
         recFunc(root.right, ans);
+    }
+    //Iterative solution using loops and stack without recursion
+    /*
+    This code iteratively traverses a binary tree in an preorder manner using a stack to keep track of the nodes to be processed.
+     It ensures that we visit the left subtree, then the current node, and then the right subtree, just like the recursive approach,
+     but without the use of recursion. This approach is more memory-efficient and avoids stack overflow errors for large trees.
+     */
+    public List<Integer> preorderTraversalSol2(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr=root;
+        while(curr != null || !stack.isEmpty()){
+            while(curr != null){
+                stack.push(curr);
+                ans.add(curr.val);
+                curr=curr.left;
+            }
+
+            curr = stack.pop();
+            curr=curr.right;
+        }
+        return ans;
     }
 }
