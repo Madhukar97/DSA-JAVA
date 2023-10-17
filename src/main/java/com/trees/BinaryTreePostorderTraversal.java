d1a8212b6ca9d90e4b84ed62ce386602a2e89f42
Madhukar <madhukar.tatiraju@gmail.com>
2023-10-18 03:03:21 +0530

    Added BinaryTreePostorderTraversal problem sol

--- src/main/java/com/trees/BinaryTreePostorderTraversal.java
+++ src/main/java/com/trees/BinaryTreePostorderTraversal.java
@@ -1,7 +1,9 @@
 package com.trees;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 //145. Binary Tree Postorder Traversal
 public class BinaryTreePostorderTraversal {
@@ -34,5 +36,29 @@ public class BinaryTreePostorderTraversal {
         recFunc(root.left, ans);
         recFunc(root.right, ans);
         ans.add(root.val);
+    }
+    //Iterative solution using loops and stack without recursion
+    /*
+    This code iteratively traverses a binary tree in an postorder manner using a stack to keep track of the nodes to be processed.
+     It ensures that we visit the right subtree, then the current node, and then the left subtree, just like the recursive approach,
+     but without the use of recursion. This approach is more memory-efficient and avoids stack overflow errors for large trees.
+     Finally reverse the ans and return
+     */
+    public List<Integer> postorderTraversalSol2(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr= root;
+        while(curr != null || !stack.isEmpty()){
+            while(curr != null){
+                ans.add(curr.val);
+                stack.push(curr);
+                curr=curr.right;
+            }
+
+            curr=stack.pop();
+            curr=curr.left;
+        }
+        Collections.reverse(ans);
+        return ans;
     }
 }
