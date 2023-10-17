30de61bf693ed86e9763f281be8dab91bcbaea1d
Madhukar <madhukar.tatiraju@gmail.com>
2023-10-18 03:43:51 +0530

    Added BinaryTreePreorderTraversal problem sol

--- src/main/java/com/trees/BinaryTreePreorderTraversal.java
+++ src/main/java/com/trees/BinaryTreePreorderTraversal.java
@@ -58,4 +58,31 @@ public class BinaryTreePreorderTraversal {
         }
         return ans;
     }
+    //Morris Preorder Traversal with time O(n) and space O(1)
+    //Most Optimal
+    public List<Integer> preorderTraversalMorris(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+
+        while(curr != null ){
+            if(curr.left == null){
+                ans.add(curr.val);
+                curr=curr.right;
+            }else{
+                TreeNode prev = curr.left;
+                while(prev.right != null && prev.right != curr){
+                    prev=prev.right;
+                }
+                if(prev.right == curr){
+                    prev.right=null;
+                    curr=curr.right;
+                }else{
+                    prev.right=curr;
+                    ans.add(curr.val);
+                    curr=curr.left;
+                }
+            }
+        }
+        return ans;
+    }
 }
