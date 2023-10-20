d7da1376df51ec0fe509660e444e071a0121e85e
Madhukar <madhukar.tatiraju@gmail.com>
2023-10-21 02:17:57 +0530

    Added SearchInABinarySearchTree problem sol

---
+++ src/main/java/com/trees/SearchInABinarySearchTree.java
@@ -0,0 +1,34 @@
+package com.trees;
+
+//700. Search in a Binary Search Tree
+//https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+public class SearchInABinarySearchTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    //use DFS with recursion : return node if found else null
+    public TreeNode searchBST(TreeNode root, int val) {
+        return rec(root,val);
+    }
+
+    public TreeNode rec(TreeNode node, int val){
+        if(node == null) return null;
+
+        if(node.val == val) return node;
+
+        TreeNode left = rec(node.left, val);
+        TreeNode right = rec(node.right, val);
+
+        if(left == null) return right;
+        return left;
+    }
+}
