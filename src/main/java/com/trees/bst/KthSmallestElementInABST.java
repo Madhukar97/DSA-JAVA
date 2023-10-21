d74d2f2061ada9206af5025d5ccf62d1a4f0d8e1
Madhukar <madhukar.tatiraju@gmail.com>
2023-10-21 21:37:55 +0530

    Added KthSmallestElementInABST problem sol

---
+++ src/main/java/com/trees/bst/KthSmallestElementInABST.java
@@ -0,0 +1,55 @@
+package com.trees.bst;
+
+import com.trees.BinaryTreeLevelOrderTraversal;
+
+import java.util.*;
+
+//230. Kth Smallest Element in a BST
+//https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+public class KthSmallestElementInABST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    //store inorder traversal(sorted list) in a array and return arr[k-1]
+    // Time O(n) and space O(n)
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        rec(root,list);
+        return list.get(k-1);
+    }
+
+    public void rec(TreeNode node, List<Integer> list){
+        if(node == null) return;
+
+        rec(node.left,list);
+        list.add(node.val);
+        rec(node.right,list);
+    }
+
+    //use DFS and recursion and carry count when count == k return ans
+    //time O(n) and space(1)
+    public int kthSmallestSol2(TreeNode root, int k) {
+        int[] count = new int[1];
+        return rec(root,k,count).val;
+    }
+
+    public TreeNode rec(TreeNode node, int k, int[] count){
+        if(node == null) return null;
+
+        TreeNode left = rec(node.left,k,count);
+        count[0]+=1;
+        if(count[0] == k) return node;
+        TreeNode right = rec(node.right,k,count);
+        if(left == null) return right;
+        return left;
+    }
+}
