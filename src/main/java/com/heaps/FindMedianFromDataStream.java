1c4af14aec4fb59ea3e3dd89ecff7f474403c3f4
Madhukar <madhukar.tatiraju@gmail.com>
2023-10-22 21:27:46 +0530

    Added FindMedianFromDataStream sol

--- src/main/java/com/heaps/FindMedianFromDataStream.java
+++ src/main/java/com/heaps/FindMedianFromDataStream.java
@@ -25,4 +25,41 @@ public class FindMedianFromDataStream {
             else return (list.get(list.size()/2)+list.get(list.size()/2-1))/2;
         }
     }
+
+    //sol 2 using heaps
+    class MedianFinder2 {
+        private PriorityQueue<Integer> minHeap;
+        private PriorityQueue<Integer> maxHeap;
+
+        public MedianFinder2() {
+            minHeap = new PriorityQueue<>();
+            maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        }
+
+        public void addNum(int num) {
+            if (maxHeap.size() == 0 || maxHeap.peek() >= num) {
+                maxHeap.offer(num);
+            } else {
+                minHeap.offer(num);
+            }
+            balance();
+        }
+
+        public double findMedian() {
+            if (maxHeap.size() > minHeap.size()) {
+                return maxHeap.peek();
+            } else if (maxHeap.size() < minHeap.size()) {
+                return minHeap.peek();
+            }
+            return (minHeap.peek() + maxHeap.peek()) / 2.0;
+        }
+
+        private void balance() {
+            if (maxHeap.size() - minHeap.size() > 1) {
+                minHeap.offer(maxHeap.poll());
+            } else if (minHeap.size() - maxHeap.size() > 1) {
+                maxHeap.offer(minHeap.poll());
+            }
+        }
+    }
 }
