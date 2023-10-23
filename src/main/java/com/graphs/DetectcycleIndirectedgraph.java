8c885122764ce14c99a6b3fed737a304f2027d36
Madhukar <madhukar.tatiraju@gmail.com>
2023-10-23 22:51:38 +0530

    Added DetectcycleIndirectedgraph sol

--- src/main/java/com/graphs/DetectcycleIndirectedgraph.java
+++ src/main/java/com/graphs/DetectcycleIndirectedgraph.java
@@ -1,6 +1,6 @@
 package com.graphs;
 
-import java.util.ArrayList;
+import java.util.*;
 
 //Detect cycle in a directed graph
 //https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1#
@@ -34,6 +34,39 @@ public class DetectcycleIndirectedgraph {
             }
         }
         recS[v] = false;
+        return false;
+    }
+//https://www.codingninjas.com/studio/problems/1062626?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+    public static boolean detectCycleInDirectedGraph(int n, ArrayList < ArrayList < Integer >> edges) {
+        int[] vis = new int[n+1];
+        int[] dfsvis = new int[n+1];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<=n;i++) adj.add(new ArrayList<>());
+        for(List<Integer> edge : edges){
+            int x1 = edge.get(0);
+            int x2 = edge.get(1);
+            adj.get(x1).add(x2);
+        }
+        // System.out.println("ADJ list : " + adj.toString());
+        for(int i=1;i<=n;i++){
+            if(vis[i]==0){
+                if(dfsDetectCycle(adj,i,vis,dfsvis)) return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean dfsDetectCycle(ArrayList<ArrayList<Integer>> adj, int index, int[] vis, int[] dfsvis){
+        vis[index]=1;
+        dfsvis[index]=1;
+
+        for(int neighbour : adj.get(index)){
+            if(vis[neighbour]==0){
+                if(dfsDetectCycle(adj, neighbour, vis, dfsvis)) return true;
+            }
+            else if(vis[neighbour]==1 && dfsvis[neighbour]==1) return true;
+        }
+        dfsvis[index]=0;
         return false;
     }
 }
