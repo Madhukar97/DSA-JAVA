e8255f2e38e477b95d16866011bc7bf7d4e9955f
Madhukar <madhukar.tatiraju@gmail.com>
2023-11-09 05:25:21 +0530

    Added CyclicLinkedList2 sol

--- src/main/java/com/linkedlistproblems/CyclicLinkedList2.java
+++ src/main/java/com/linkedlistproblems/CyclicLinkedList2.java
@@ -68,4 +68,27 @@ public class CyclicLinkedList2 {
         }
         return null;
     }
+
+    //Optimal sol 2
+    //Revision 2
+    public ListNode detectCycleR2(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) break;
+        }
+
+        if(slow != fast) return null;
+
+        slow = head;
+        while(slow != fast){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return slow;
+    }
 }
