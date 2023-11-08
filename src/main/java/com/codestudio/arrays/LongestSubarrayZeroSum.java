630293e5b742c8bacc0bdc3953c9091189ef40e3
Madhukar <madhukar.tatiraju@gmail.com>
2023-11-09 02:19:44 +0530

    Added LongestSubarrayZeroSum sol

--- src/main/java/com/codestudio/arrays/LongestSubarrayZeroSum.java
+++ src/main/java/com/codestudio/arrays/LongestSubarrayZeroSum.java
@@ -28,4 +28,27 @@ public class LongestSubarrayZeroSum {
         }
         return maxLen;
     }
+
+    //Optimal sol for +ve's and -ve's
+    //Revision 2
+    public static int LongestSubsetWithZeroSumR2(ArrayList<Integer> arr) {
+        int n=arr.size();
+        Map<Integer,Integer> prefixSumMap = new HashMap<>();
+        int sum=0;
+        int maxLen=0;
+
+        for(int i=0;i<arr.size();i++){
+            sum+=arr.get(i);
+            if(sum == 0){
+                maxLen = Math.max(maxLen, i+1);
+            }
+            if(prefixSumMap.containsKey(sum)){
+                maxLen = Math.max(maxLen, i-prefixSumMap.get(sum));
+            }
+            if(!prefixSumMap.containsKey(sum)){
+                prefixSumMap.put(sum,i);
+            }
+        }
+        return maxLen;
+    }
 }
