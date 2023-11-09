eb897dfe9ac23afc77655e7de2b6e0b769b87720
Madhukar <madhukar.tatiraju@gmail.com>
2023-11-10 02:37:51 +0530

    Added ReverseNodesInKGroup sol

--- src/main/java/com/linkedlistproblems/ReverseNodesInKGroup.java
+++ src/main/java/com/linkedlistproblems/ReverseNodesInKGroup.java
@@ -38,4 +38,35 @@ public class ReverseNodesInKGroup {
         }
         return dummy.next;
     }
+
+    //Optimal sol
+    //Revision 2
+    public ListNode reverseKGroupR2(ListNode head, int k) {
+        ListNode root = new ListNode();
+        root.next=head;
+        ListNode prev = root;
+        ListNode end = head;
+        ListNode curr = null;
+        if(prev != null) curr = prev.next;
+        ListNode next = null;
+        if(curr != null) next = curr.next;
+
+        while(end != null){
+            for(int i=0;i<k;i++) {
+                if(end == null) return root.next;
+                end=end.next;
+            }
+            curr=prev.next;
+            next=curr.next;
+
+            while(next != end){
+                curr.next=next.next;
+                next.next=prev.next;
+                prev.next=next;
+                next = curr.next;
+            }
+            prev=curr;
+        }
+        return root.next;
+    }
 }
