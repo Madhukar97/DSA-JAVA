97a5af9ac615cab2fbdd3e7c7e7acd4800b3ddce
Madhukar <madhukar.tatiraju@gmail.com>
2023-11-14 01:40:07 +0530

    Added NextGreaterElementI sol

--- src/main/java/com/stackandqueue/NextGreaterElementI.java
+++ src/main/java/com/stackandqueue/NextGreaterElementI.java
@@ -1,6 +1,7 @@
 package com.stackandqueue;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 //496. Next Greater Element I
@@ -50,6 +51,29 @@ public class NextGreaterElementI {
             map.put(nums2[i], nge[i]);
         }
 
+        int[] ans = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            ans[i] = map.get(nums1[i]);
+        }
+        return ans;
+    }
+
+    //Revision 2
+    public int[] nextGreaterElementR2(int[] nums1, int[] nums2) {
+        int[] nge = new int[nums2.length];
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek() <= nums2[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()) nge[i] = -1;
+            else nge[i]=stack.peek();
+            map.put(nums2[i], nge[i]);
+            stack.push(nums2[i]);
+        }
+        // System.out.println("NGE : " + Arrays.toString(nge));
         int[] ans = new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
             ans[i] = map.get(nums1[i]);
