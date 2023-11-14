0a0a8c94b37554510d5edffbc228e4212406b255
Madhukar <madhukar.tatiraju@gmail.com>
2023-11-15 03:23:17 +0530

    Added RomanToInteger sol

--- src/main/java/com/strings/RomanToInteger.java
+++ src/main/java/com/strings/RomanToInteger.java
@@ -33,4 +33,29 @@ public class RomanToInteger {
         ans+=map.get(s.charAt(s.length()-1));
         return ans;
     }
+
+    //Revision 2
+    public int romanToIntR2(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int ans=0;
+        int i=0;
+        while(i<s.length()){
+            if(i+1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+                ans+=map.get(s.charAt(i+1)) - map.get(s.charAt(i));
+                i+=2;
+            }else{
+                ans+=map.get(s.charAt(i));
+                i++;
+            }
+        }
+        return ans;
+    }
 }
