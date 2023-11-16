fcb819f9a1b71ab635b0660312f8c9c704fc3888
Madhukar <madhukar.tatiraju@gmail.com>
2023-11-17 02:18:23 +0530

    Added AverageOfLevelsInBinaryTree  sol

--- src/main/java/com/trees/AverageOfLevelsInBinaryTree.java
+++ src/main/java/com/trees/AverageOfLevelsInBinaryTree.java
@@ -1,6 +1,6 @@
 package com.trees;
 
-import java.util.;
+import java.util.*;
 
 //637. Average of Levels in Binary Tree
 //https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
