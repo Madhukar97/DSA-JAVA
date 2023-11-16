917a8e017ba9a96fba6b25a4622fbebd30d8cee9
Madhukar <madhukar.tatiraju@gmail.com>
2023-11-17 02:17:51 +0530

    Added AverageOfLevelsInBinaryTree  sol

---
+++ src/main/java/com/trees/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,40 @@
+package com.trees;
+
+import java.util.;
+
+//637. Average of Levels in Binary Tree
+//https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+public class AverageOfLevelsInBinaryTree {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+     }
+ }
+    //using level order traversal
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int n = q.size();
+            long sum=0;
+
+            for(int i=0;i<n;i++){
+                TreeNode node = q.poll();
+                sum+=node.val;
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+            }
+            ans.add(sum/(double)n);
+        }
+        return ans;
+    }
+}
