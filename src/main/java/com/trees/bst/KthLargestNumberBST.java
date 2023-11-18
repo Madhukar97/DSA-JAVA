891e7a829a727abc56b88d01ab004a3e10e22692
Madhukar <madhukar.tatiraju@gmail.com>
2023-11-19 00:12:28 +0530

    Added KthLargestNumberBST sol

--- src/main/java/com/trees/bst/KthLargestNumberBST.java
+++ src/main/java/com/trees/bst/KthLargestNumberBST.java
@@ -41,4 +41,39 @@ public class KthLargestNumberBST {
         count[0]+=1;
         rec(node.right,count);
     }
+
+    //Revision 2
+
+    class Node
+    {
+        int data;
+        Node left;
+        Node right;
+        Node(int data)
+        {
+            this.data = data;
+            left=null;
+            right=null;
+        }
+    }
+    class Solution
+    {
+        // return the Kth largest element in the given BST rooted at 'root'
+        public int kthLargest(Node root,int K)
+        {
+            int[] ans = new int[2];
+            ans[0] = K;
+            rec(root,ans);
+            return ans[1];
+        }
+
+        public void rec(Node node, int[] ans){
+            if(node == null) return;
+
+            rec(node.right,ans);
+            ans[0]--;
+            if(ans[0] == 0) ans[1] = node.data;
+            rec(node.left,ans);
+        }
+    }
 }
