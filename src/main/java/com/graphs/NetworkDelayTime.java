2c70cdbf4f9e0f9729963d9fcfe6323567b9b2ff
Madhukar <madhukar.tatiraju@gmail.com>
2023-11-25 03:11:16 +0530

    Added NetworkDelayTime sol

--- src/main/java/com/graphs/NetworkDelayTime.java
+++ src/main/java/com/graphs/NetworkDelayTime.java
@@ -53,4 +53,56 @@ public class NetworkDelayTime {
             this.wt=wt;
         }
     }
+
+    //Revision 2
+    //0 based indexing sol
+    class Solution {
+        public int networkDelayTime(int[][] times, int n, int k) {
+            //Dijkstras algo
+            PriorityQueue<Node> pq = new PriorityQueue<>((n1,n2)->n1.wt-n2.wt);
+            pq.add(new Node(k-1, 0));
+
+            List<List<Node>> adj = new ArrayList<>();
+            for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+
+            for(int[] edge : times){
+                int x = edge[0]-1;
+                int y = edge[1]-1;
+                int wt = edge[2];
+                adj.get(x).add(new Node(y, wt));
+            }
+
+            int[] dist = new int[n];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            dist[k-1] = 0;
+
+            while(!pq.isEmpty()){
+                Node node = pq.poll();
+                int wt = node.wt;
+                int i = node.i;
+
+                for(Node neighbour : adj.get(i)){
+                    if(neighbour.wt + wt < dist[neighbour.i]){
+                        dist[neighbour.i] = neighbour.wt + wt;
+                        pq.add(new Node(neighbour.i, dist[neighbour.i]));
+                    }
+                }
+            }
+
+            for(int i : dist) if(i == Integer.MAX_VALUE) return -1;
+            int max = 0;
+            for(int i : dist) max=Math.max(max, i);
+            return max;
+        }
+
+        public class Node{
+            int i;
+            int wt;
+
+            public Node(int i, int w){
+                this.i=i;
+                wt=w;
+            }
+        }
+    }
 }
