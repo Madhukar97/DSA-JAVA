53f4472395d5b3e3b65c029b99ca06757a97d7b5
Madhukar <madhukar.tatiraju@gmail.com>
2023-12-25 22:33:34 +0530

    Added MaxConsecutiveOnesIII sol

--- src/main/java/com/arrays/MaxConsecutiveOnesIII.java
+++ src/main/java/com/arrays/MaxConsecutiveOnesIII.java
@@ -3,7 +3,32 @@ package com.arrays;
 //1004. Max Consecutive Ones III
 //https://leetcode.com/problems/max-consecutive-ones-iii/description/
 public class MaxConsecutiveOnesIII {
+    //Optimal sol using sliding window
     class Solution {
+        public int longestOnes(int[] nums, int k) {
+            int count=0;
+            int max=0;
+            int s=0;
+            int e=0;
+
+            while(e < nums.length){
+                if(nums[e] == 0){
+                    count++;
+                }
+
+                while(count > k){
+                    if(nums[s] == 0) count--;
+                    s++;
+                }
+                max = Math.max(max, e-s+1);
+                e++;
+            }
+            return max;
+        }
+    }
+
+    //Sol2 using sliding window
+    class Solution2 {
         public int longestOnes(int[] nums, int k) {
             int i=0;
             int j=0;
