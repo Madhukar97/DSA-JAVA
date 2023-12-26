bbfde096a9e384b3015e3983cad64a9a16dff3f4
Madhukar <madhukar.tatiraju@gmail.com>
2023-12-26 23:18:48 +0530

    Added FractionalKnapsack sol

--- src/main/java/com/greedy/FractionalKnapsack.java
+++ src/main/java/com/greedy/FractionalKnapsack.java
@@ -39,4 +39,43 @@ public class FractionalKnapsack {
         }
         return ans;
     }
+
+    //code studio sol
+    //optimal sol 2
+    public class Solution {
+        public static double maximumValue(Pair[] items, int n, int w) {
+            Arrays.sort(items, (i1,i2)->{
+                double r1 = (double)i1.value/(double)i1.weight;
+                double r2 = (double)i2.value/(double)i2.weight;
+                double diff = r2-r1;
+                if(diff == 0) return 0;
+                else if(diff > 0) return 1;
+                else return -1;
+            });
+
+            double max = 0;
+            double wt = w;
+            for(Pair item : items){
+                if(wt > 0){
+                    double remWt = Math.min(item.weight, wt);
+                    max+= remWt*(double)item.value/(double)item.weight;
+                    wt-=remWt;
+                }else {
+                    break;
+                }
+            }
+            return max;
+        }
+    }
+    class Pair
+    {
+        int weight;
+        int value;
+        Pair(int weight, int value)
+        {
+            this.weight = weight;
+            this.value = value;
+        }
+
+    }
 }
