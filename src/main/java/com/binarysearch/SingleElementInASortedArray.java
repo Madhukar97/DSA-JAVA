0e1358a8726bed7af58e72d75e645425cb0bd9b9
Madhukar <madhukar.tatiraju@gmail.com>
2023-12-27 18:52:17 +0530

    Added SingleElementInASortedArray sol

--- src/main/java/com/binarysearch/SingleElementInASortedArray.java
+++ src/main/java/com/binarysearch/SingleElementInASortedArray.java
@@ -41,4 +41,29 @@ public class SingleElementInASortedArray {
         }
         return nums[s];
     }
+
+    //Revision 2
+    //Optimal sol using binary Search
+    class Solution {
+        public int singleNonDuplicate(int[] nums) {
+            if(nums.length == 1) return nums[0];
+            int s=0;
+            int e=nums.length-1;
+            while(s<=e){
+                int mid = (s+e)/2;
+                int count = 1;
+                if(mid-1 >= 0 && nums[mid-1] == nums[mid]) count++;
+                if(mid+1 < nums.length && nums[mid] == nums[mid+1]) count++;
+                if(count == 1) return nums[mid];
+                if(mid%2==0){
+                    if(nums[mid] != nums[mid+1]) e=mid-1;
+                    else s=mid+1;
+                }else{
+                    if(nums[mid] != nums[mid-1]) e=mid-1;
+                    else s=mid+1;
+                }
+            }
+            return nums[s];
+        }
+    }
 }
