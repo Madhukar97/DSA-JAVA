569a88cb4bdf65e99190917b3c9f8bfa14d2fbf4
Madhukar <madhukar.tatiraju@gmail.com>
2024-01-06 22:23:56 +0530

    Added MinimumNumberOfOperationsToMakeXAndYEqual sol

--- src/main/java/com/graphs/MinimumNumberOfOperationsToMakeXAndYEqual.java
+++ src/main/java/com/graphs/MinimumNumberOfOperationsToMakeXAndYEqual.java
@@ -11,7 +11,7 @@ public class MinimumNumberOfOperationsToMakeXAndYEqual {
             if(x==y) return 0;
             if(x < y) return y-x;
 
-            PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2)->n1.ops-n2.ops);
+            Queue<Node> pq = new LinkedList<>();
             pq.add(new Node(x, 0));
             Set<Integer> set = new HashSet<>();
 
@@ -19,31 +19,26 @@ public class MinimumNumberOfOperationsToMakeXAndYEqual {
                 Node node = pq.poll();
                 int num = node.num;
                 int ops = node.ops;
-                // System.out.println("num : " + num + " , ops : " + ops);
+
                 if(num == y) return ops;
-                if(num > 10000 || num < 0) continue;
 
                 if(num%11 == 0 && !set.contains(num/11)){
                     int newNum = num/11;
-                    if(newNum == y) return ops+1;
                     pq.add(new Node(newNum, ops+1));
                     set.add(newNum);
                 }
                 if(num%5 == 0 && !set.contains(num/5)){
                     int newNum = num/5;
-                    if(newNum == y) return ops+1;
                     pq.add(new Node(newNum, ops+1));
                     set.add(newNum);
                 }
                 if(!set.contains(num+1)){
                     pq.add(new Node(num+1, ops+1));
                     set.add(num+1);
-                    if(num+1 == y) return ops+1;
                 }
                 if(!set.contains(num-1)){
                     pq.add(new Node(num-1, ops+1));
                     set.add(num-1);
-                    if(num-1 == y) return ops+1;
                 }
             }
             return 0;
