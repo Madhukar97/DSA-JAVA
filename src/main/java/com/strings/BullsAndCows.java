15184a6060f9137abf35f2d83f0204fb8e62ae1e
Madhukar <madhukar.tatiraju@gmail.com>
2024-01-10 01:48:25 +0530

    Added BullsAndCows sol

--- src/main/java/com/strings/BullsAndCows.java
+++ src/main/java/com/strings/BullsAndCows.java
@@ -9,15 +9,15 @@ public class BullsAndCows {
         public String getHint(String secret, String guess) {
             int bulls=0;
             int cows=0;
-            Map<Integer,Integer> map = new HashMap<>();
+            Map<Character,Integer> map = new HashMap<>();
 
             for(int i=0;i<secret.length();i++){
-                int digit = secret.charAt(i)-'0';
+                char digit = secret.charAt(i);
                 map.put(digit, map.getOrDefault(digit, 0)+1);
             }
 
             for(int i=0;i<guess.length();i++){
-                int digit = guess.charAt(i) - '0';
+                char digit = guess.charAt(i);
                 if(secret.charAt(i) == guess.charAt(i)){
                     bulls++;
                     map.put(digit, map.get(digit)-1);
@@ -26,7 +26,7 @@ public class BullsAndCows {
             }
 
             for(int i=0;i<guess.length();i++){
-                int digit = guess.charAt(i) - '0';
+                char digit = guess.charAt(i);
                 if(secret.charAt(i) != guess.charAt(i) && map.containsKey(digit)){
                     cows++;
                     map.put(digit, map.get(digit)-1);
