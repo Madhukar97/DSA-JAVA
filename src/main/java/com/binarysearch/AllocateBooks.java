6033f0a324a4532214b89ad0989c7a68180aeba7
Madhukar <madhukar.tatiraju@gmail.com>
2025-03-28 02:39:24 +0530

    Added AllocateBooks sol

--- src/main/java/com/binarysearch/AllocateBooks.java
+++ src/main/java/com/binarysearch/AllocateBooks.java
@@ -37,4 +37,34 @@ public class AllocateBooks {
             return low;
         }
     }
+
+    // Revision 5
+    public class Solution2 {
+        public static int findPages(ArrayList<Integer> arr, int n, int m) {
+            // Write your code here.
+            if(m > n) return  -1;
+            int min=arr.get(0);
+            int max=0;
+            for(int pgs : arr) {
+                min = Math.max(min, pgs);
+                max += pgs;
+            }
+            while(min<=max){
+                int mid = min+(max-min)/2;
+                int stds = 0;
+                int totalPgs=0;
+                for(int pgs : arr){
+                    totalPgs += pgs;
+                    if(totalPgs > mid){
+                        totalPgs = pgs;
+                        stds++;
+                    }
+                }
+                if(totalPgs > 0) stds++;
+                if(stds <= m) max=mid-1;
+                else min=mid+1;
+            }
+            return min;
+        }
+    }
 }
