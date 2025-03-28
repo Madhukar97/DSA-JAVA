884eadb05a22f1bedddc253166f4e495a9cecbf7
Madhukar <madhukar.tatiraju@gmail.com>
2025-03-29 01:58:15 +0530

    Added BinaryTreeLevelOrderTraversal sol

--- src/main/java/com/trees/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/com/trees/BinaryTreeLevelOrderTraversal.java
@@ -57,4 +57,28 @@ public class BinaryTreeLevelOrderTraversal {
         outer.add(inner);
         return outer;
     }
+
+    // Revision 5
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> ans = new ArrayList<>();
+            if(root == null) return ans;
+            Queue<TreeNode> q = new LinkedList<>();
+            q.add(root);
+            List<Integer> lvl = new ArrayList<>();
+
+            while(!q.isEmpty()){
+                int size = q.size();
+                lvl = new ArrayList<>();
+                for(int i=0;i<size;i++){
+                    TreeNode curr = q.poll();
+                    if(curr.left != null) q.add(curr.left);
+                    if(curr.right != null) q.add(curr.right);
+                    lvl.add(curr.val);
+                }
+                ans.add(lvl);
+            }
+            return ans;
+        }
+    }
 }
