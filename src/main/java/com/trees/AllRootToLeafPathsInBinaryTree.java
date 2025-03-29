04cb333ab04ba39e1902022e12dede34b0afb19c
Madhukar <madhukar.tatiraju@gmail.com>
2025-03-29 17:25:14 +0530

    Added AllRootToLeafPathsInBinaryTree sol

--- src/main/java/com/trees/AllRootToLeafPathsInBinaryTree.java
+++ src/main/java/com/trees/AllRootToLeafPathsInBinaryTree.java
@@ -3,6 +3,7 @@ package com.trees;
 import java.util.*;
 
 //All Root to Leaf Paths In Binary Tree.
+//https://www.geeksforgeeks.org/problems/root-to-leaf-paths/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=root-to-leaf-paths
 //https://www.codingninjas.com/studio/problems/all-root-to-leaf-paths-in-binary-tree._983599?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=1
 public class AllRootToLeafPathsInBinaryTree {
     class BinaryTreeNode {
@@ -59,5 +60,29 @@ public class AllRootToLeafPathsInBinaryTree {
 
         rec(node.left, ans, path+node.data+" ");
         rec(node.right,ans, path+node.data+" ");
+    }
+
+    // Revision 5
+    class Solution3 {
+        public static ArrayList<ArrayList<Integer>> Paths(BinaryTreeNode root) {
+            // code here
+            ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+            ArrayList<Integer> path = new ArrayList<>();
+            inorder(root, path, ans);
+            return ans;
+        }
+
+        public static void inorder(BinaryTreeNode node, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> ans){
+            if(node == null) return;
+            path.add(node.data);
+            if(node.left == null && node.right == null){
+                ans.add(new ArrayList<>(path));
+                path.remove(path.size()-1);
+                return;
+            }
+            inorder(node.left, path, ans);
+            inorder(node.right, path, ans);
+            path.remove(path.size()-1);
+        }
     }
 }
