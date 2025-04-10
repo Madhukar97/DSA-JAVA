16dc90d1eb2e15455c918457bc2f6c409a38c56a
Madhukar <madhukar.tatiraju@gmail.com>
2025-04-11 03:37:02 +0530

    Added PermutationsII sol

--- src/main/java/com/recursion/PermutationsII.java
+++ src/main/java/com/recursion/PermutationsII.java
@@ -39,4 +39,32 @@ public class PermutationsII {
         nums[l] = nums[i];
         nums[i] = temp;
     }
+
+    // Revision 5
+    class Solution {
+        public List<List<Integer>> permuteUnique(int[] nums) {
+            int[] vis = new int[nums.length];
+            List<List<Integer>> ans = new ArrayList<>();
+            findAllPermutations(0, nums, ans, vis, new ArrayList<>());
+            return ans;
+        }
+
+        private void findAllPermutations(int index, int[] nums, List<List<Integer>> ans, int[] vis, List<Integer> permutation) {
+            if(permutation.size() == nums.length){
+                if(!ans.contains(permutation)) ans.add(new ArrayList<>(permutation));
+                return;
+            }
+            if(index == nums.length) return;
+            // not pick
+            findAllPermutations(index+1, nums, ans, vis, permutation);
+            // pick
+            if(vis[index] == 0) {
+                vis[index]=1;
+                permutation.add(nums[index]);
+                findAllPermutations(0, nums, ans, vis, permutation);
+                permutation.remove(permutation.size()-1);
+                vis[index]=0;
+            }
+        }
+    }
 }
