5664e388109bfffdd9d8cfff3baf517cc23cdd92
Madhukar <madhukar.tatiraju@gmail.com>
2025-04-16 01:14:09 +0530

    Added FindFirstAndLastPositionOfElementInSortedArray sol

--- src/main/java/com/binarysearch/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/com/binarysearch/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -40,4 +40,36 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
             return index;
         }
     }
+
+    // Revision 5
+    class Solution2 {
+        public int[] searchRange(int[] nums, int target) {
+            int floor = floor(nums, target);
+            int ceil = ceil(nums, target);
+            return new int[]{floor,ceil};
+        }
+
+        private int floor(int[] nums, int target){
+            int s=0,e=nums.length-1,index=-1;
+            while(s <= e){
+                int mid = s+(e-s)/2;
+                if(nums[mid] == target) index=mid;
+                if(nums[mid] < target){
+                    s=mid+1;
+                }else e=mid-1;
+            }
+            return index;
+        }
+        private int ceil(int[] nums, int target){
+            int s=0,e=nums.length-1,index=-1;
+            while(s <= e){
+                int mid = s+(e-s)/2;
+                if(nums[mid] == target) index=mid;
+                if(nums[mid] > target){
+                    e=mid-1;
+                }else s=mid+1;
+            }
+            return index;
+        }
+    }
 }
