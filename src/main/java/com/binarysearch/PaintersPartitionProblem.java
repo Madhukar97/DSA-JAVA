317f7b18f3eb6f52d475b878716d0a059e8a8080
Madhukar <madhukar.tatiraju@gmail.com>
2025-04-18 05:07:10 +0530

    Added PaintersPartitionProblem sol

--- src/main/java/com/binarysearch/PaintersPartitionProblem.java
+++ src/main/java/com/binarysearch/PaintersPartitionProblem.java
@@ -3,6 +3,7 @@ package com.binarysearch;
 import java.util.ArrayList;
 
 // Painter's Partition Problem
+//https://www.naukri.com/code360/problems/painter-s-partition-problem_1089557?leftPanelTabValue=PROBLEM
 //https://www.codingninjas.com/studio/problems/painter-s-partition-problem_1089557?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
 public class PaintersPartitionProblem {
     //Similar to Allocate books
@@ -34,5 +35,32 @@ public class PaintersPartitionProblem {
             else low = mid+1;
         }
         return low;
+    }
+    // Revision 5
+    public class Solution2 {
+        public static int findLargestMinDistance(ArrayList<Integer> boards, int k) {
+            int min=0;
+            int max=0;
+            for(int b : boards) {
+                min = Math.max(min, b);
+                max += b;
+            }
+            while(min <= max){
+                int mid = min + (max-min)/2;
+                int units=0;
+                int pntrs=0;
+                for(int b : boards){
+                    units += b;
+                    if(units > mid){
+                        units = b;
+                        pntrs++;
+                    }
+                }
+                if(units > 0) pntrs++;
+                if(pntrs > k) min=mid+1;
+                else max=mid-1;
+            }
+            return min;
+        }
     }
 }
