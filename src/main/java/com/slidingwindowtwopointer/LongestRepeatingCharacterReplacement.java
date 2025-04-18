cbe938e6d2f0c43d61ff0808b7da7172b5492058
Madhukar <madhukar.tatiraju@gmail.com>
2025-04-19 04:44:25 +0530

    Added LongestRepeatingCharacterReplacement sol

--- src/main/java/com/slidingwindowtwopointer/LongestRepeatingCharacterReplacement.java
+++ src/main/java/com/slidingwindowtwopointer/LongestRepeatingCharacterReplacement.java
@@ -1,5 +1,7 @@
 package com.slidingwindowtwopointer;
 
+import java.util.*;
+
 //424. Longest Repeating Character Replacement
 //https://leetcode.com/problems/longest-repeating-character-replacement/description/
 public class LongestRepeatingCharacterReplacement {
@@ -55,6 +57,33 @@ public class LongestRepeatingCharacterReplacement {
             int max=0;
             for(int i : chars) max =  Math.max(max,i);
             return max;
+        }
+    }
+    // Better sol using HashMap
+    class Solution3 {
+        public int characterReplacement(String s, int k) {
+            Map<Character, Integer> map = new HashMap<>();
+            int i=0,j=0,max=0;
+            while(j < s.length()){
+                map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0)+1);
+                if(isValidWindow(map, k)) max = Math.max(max, j-i+1);
+                else {
+                    if(map.get(s.charAt(i)) == 1) map.remove(s.charAt(i));
+                    else map.put(s.charAt(i), map.get(s.charAt(i))-1);
+                    i++;
+                }
+                j++;
+            }
+            return max;
+        }
+
+        private boolean isValidWindow(Map<Character, Integer> map, int k){
+            int maxFreq = 0, totalFreq=0;
+            for(int val : map.values()){
+                totalFreq+=val;
+                maxFreq = Math.max(maxFreq, val);
+            }
+            return totalFreq-maxFreq <= k;
         }
     }
 }
