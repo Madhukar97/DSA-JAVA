dab76c024985015355a0b70204a658caff2d2883
Madhukar <madhukar.tatiraju@gmail.com>
2025-04-20 02:27:46 +0530

    Added NumberOfSubstringsContainingAllThreeCharacters sol

--- src/main/java/com/slidingwindowtwopointer/NumberOfSubstringsContainingAllThreeCharacters.java
+++ src/main/java/com/slidingwindowtwopointer/NumberOfSubstringsContainingAllThreeCharacters.java
@@ -1,5 +1,7 @@
 package com.slidingwindowtwopointer;
 
+import java.util.*;
+
 //1358. Number of Substrings Containing All Three Characters
 //https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/
 public class NumberOfSubstringsContainingAllThreeCharacters {
@@ -25,6 +27,49 @@ public class NumberOfSubstringsContainingAllThreeCharacters {
         public int findCount(int[] chars){
             int count=0;
             for(int i : chars) if(i > 0) count++;
+            return count;
+        }
+    }
+    // Revision 5
+    class Solution2 {
+        public int numberOfSubstrings(String s) {
+            return countSubStrings(s, 3) - countSubStrings(s, 2);
+        }
+        private int countSubStrings(String s, int k){
+            int l=0,r=0,count=0;
+            Map<Character,Integer> map = new HashMap<>();
+            while(r < s.length()){
+                map.put(s.charAt(r), map.getOrDefault(s.charAt(r),0)+1);
+                while(map.size() > k) {
+                    if(map.get(s.charAt(l)) == 1) map.remove(s.charAt(l));
+                    else map.put(s.charAt(l), map.get(s.charAt(l))-1);
+                    l++;
+                }
+                if(map.size() <= k) count+=r-l+1;
+                r++;
+            }
+            return count;
+        }
+    }
+
+    class Solution3 {
+        public int numberOfSubstrings(String s) {
+            return countSubStrings(s, 3) - countSubStrings(s, 2);
+        }
+        private int countSubStrings(String s, int k){
+            int l=0,r=0,count=0;
+            int[] chars = new int[3];
+            while(r < s.length()){
+                chars[s.charAt(r)-'a']++;
+                while(count(chars) > k) chars[s.charAt(l++)-'a']--;
+                count+=r-l+1;
+                r++;
+            }
+            return count;
+        }
+        private int count(int[] chars){
+            int count=0;
+            for(int c : chars) if(c > 0) count++;
             return count;
         }
     }
