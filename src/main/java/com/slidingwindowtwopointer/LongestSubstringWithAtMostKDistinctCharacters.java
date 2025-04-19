f7d4ccb43666d2c4aa4e97fe17d6fbd2d648c5ab
Madhukar <madhukar.tatiraju@gmail.com>
2025-04-20 02:46:15 +0530

    Added kDistinctChars sol

--- src/main/java/com/slidingwindowtwopointer/LongestSubstringWithAtMostKDistinctCharacters.java
+++ src/main/java/com/slidingwindowtwopointer/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -1,14 +1,12 @@
 package com.slidingwindowtwopointer;
 
-import java.util.HashMap;
+import java.util.*;
 
 // Longest Substring with At Most K Distinct Characters
 //https://www.naukri.com/code360/problems/distinct-characters_2221410?leftPanelTabValue=PROBLEM
 public class LongestSubstringWithAtMostKDistinctCharacters {
     public class Solution {
-
         public static int kDistinctChars(int k, String str) {
-            // Write your code here
             HashMap<Character,Integer> map = new HashMap<>();
             int l=0;
             int r=0;
@@ -25,6 +23,25 @@ public class LongestSubstringWithAtMostKDistinctCharacters {
                 r++;
             }
             return max;
+        }
+    }
+    // Revision 5
+    public class Solution2 {
+
+        public static int kDistinctChars(int k, String str) {
+            int s=0,e=0,maxLength=0;
+            Map<Character,Integer> map = new HashMap<>();
+            while(e < str.length()){
+                map.put(str.charAt(e), map.getOrDefault(str.charAt(e),0)+1);
+                if(map.size() <= k) maxLength = Math.max(maxLength, e-s+1);
+                else {
+                    if(map.get(str.charAt(s)) == 1) map.remove(str.charAt(s));
+                    else map.put(str.charAt(s), map.get(str.charAt(s))-1);
+                    s++;
+                }
+                e++;
+            }
+            return maxLength;
         }
 
     }
